2e553fa3273e6c87f2636ed987c6288d1b37d819
Onuora Wright <onuora.wright@gmail.com>
2024-05-30 22:25:49 +0100

    Deleted comments in strings methods task

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -8,21 +8,17 @@ public class App {
     public static String concatenate(String word1, String word2, String word3) {
         // TODO: Write code that concatenates the input parameters and returns the result
         return word1 + word2 + word3;
-        // ALTERNATIVE OPTION HERE = return word1.concat(word2).concat(word3);
-        //throw new RuntimeException("Not implemented");
-
+        // ALTERNATIVE OPTION HERE =return word1.concat(word2).concat(word3);
     }
 
     public static Boolean areEqual(String word1, String word2) {
         // TODO: Write code to determine whether the input parameters are equal strings
         return word1.equals(word2);
-        //throw new RuntimeException("Not implemented");
     }
 
     public static String format(String item, int quantity, double price) {
         // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
         return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
-        //throw new RuntimeException("Not implemented");
     }
 }
 
