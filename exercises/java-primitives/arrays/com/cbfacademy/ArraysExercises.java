61bb1abedafa8fcc01757b305e9ae321e1731ab6
Onuora Wright <onuora.wright@gmail.com>
2024-06-02 21:52:49 +0100

    Created ArraysExercises.java file

---
+++ exercises/java-primitives/arrays/com/cbfacademy/ArraysExercises.java
@@ -0,0 +1,11 @@
+package com.cbfacademy;
+
+public class ArraysExercises {
+    public static void main (String... args) {
+        int[] eightIntegers = {1, 2, 3, 4, 5, 6, 7, 8};
+
+
+        
+        System.out.println(eightIntegers[4]);
+    }
+}
