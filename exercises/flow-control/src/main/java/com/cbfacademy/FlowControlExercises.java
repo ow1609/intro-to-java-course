ef8eb3b93be841afb33738e6cd7bacfa02cd1248
Onuora Wright <onuora.wright@gmail.com>
2024-06-25 14:29:52 +0100

    Completed sumOfOddsAndSumOfEvens method and reverse method

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,6 +1,8 @@
 package com.cbfacademy;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -61,14 +63,34 @@ public class FlowControlExercises {
         //  - determines the sum of all the odd numbers in the list
         //  - returns a map with two entries:
         //      {"SumOfEvens", calculatedSumOfEvens}, {"SumOfOdds", calculatedSumOfOdds}
-        throw new RuntimeException("Not implemented");
-    }
+        List<Integer> numbers1to100 = new ArrayList<>();
+        for (int i = 1 ; i <= 100 ; i++) {
+            numbers1to100.add(i);
+        }
+        int sumOfEvens = 0;
+        int sumOfOdds = 0;
 
+        for (int i = 0; i < numbers1to100.size(); i++) {
+            if (numbers1to100.get(i) % 2 == 0) {
+                sumOfEvens += numbers1to100.get(i);
+            } else {
+                sumOfOdds += numbers1to100.get(i);
+            }}
+
+        Map<String, Integer> result = new HashMap<>();
+        result.put("SumOfEvens", sumOfEvens);
+        result.put("SumOfOdds", sumOfOdds);
+
+        return result;
+        }
+
+        
     public List<Integer> reverse(ArrayList<Integer> numbers) {
         // TODO - Implement this method such that
         //  - it takes an array list of integers
         //  - it returns the list in reverse order
-        throw new RuntimeException("Not implemented");
+        Collections.reverse(numbers);
+        return numbers;
     }
 
     public String getName() {
