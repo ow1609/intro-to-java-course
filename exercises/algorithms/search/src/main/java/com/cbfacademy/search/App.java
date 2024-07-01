77b75d71ddff801c4798450ea39320c7413c2519
Onuora Wright <onuora.wright@gmail.com>
2024-07-01 21:23:57 +0100

    Started code for BinarySearch. Currently requires  a bug fix as to get it to return -1 if the target is not in the array

--- exercises/algorithms/search/src/main/java/com/cbfacademy/search/App.java
+++ exercises/algorithms/search/src/main/java/com/cbfacademy/search/App.java
@@ -1,7 +1,20 @@
 package com.cbfacademy.search;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Sorted Array is : ");
+        int[] arr1 = new int []{1, 3, 5, 8, 9, 15, 23};
+        System.out.println(Arrays.toString(arr1));
+        BinarySearch search1 = new BinarySearch();
+        System.out.println("Target to search for is : 8");
+        System.out.println("Target position is: " + search1.search( arr1, 8));
+
+        System.out.println("");
+        System.out.println("Target to search for is : 7");
+        System.out.println("Target position is: " + search1.search(arr1, 7));
+    
+
     }
 }
---
+++ exercises/algorithms/search/src/main/java/com/cbfacademy/search/BinarySearch.java
@@ -0,0 +1,48 @@
+package com.cbfacademy.search;
+
+public class BinarySearch implements Search {
+
+    public int search(int[] sortedArray, int target) {
+
+        int low = 0;
+        int high = sortedArray.length -1;
+        int mid = (Math.floorDiv((low + high), 2));
+            
+        while (low <= high) {
+            if (target == sortedArray[mid]) {
+                return mid;
+            } else if (target > sortedArray[mid]) {
+                //target could be on the right side of the array
+                low = mid + 1;
+            } else {
+                // target could be on the left side of the array
+                high = mid - 1; 
+            }
+        } 
+        return -1;
+        
+    } 
+}
+
+/**
+ * PSEUDOCODE
+ * do until the pointers low and high meet each other.
+    //find the index at the mid point of the given array (add the lowest index to the highest index and divide by two, e.g. 0 + 10 / 2 = 5)
+    mid = (low + high)/2
+    //if the target value is the mid point element
+    if (target == arr[mid])
+        // return the index number for the mid point of the array 
+        return mid
+    // if the target value is higher than the value of the mid point element 
+    else if (target > arr[mid]) // target could be on the right side of the array
+        // set the low index to the right of the middle 
+        low = mid + 1
+    // in all other cases
+    else                       // target could be on the left side of the array
+        // set the high index to the left of the middle
+        high = mid - 1
+ * 
+ */
+
+
+
